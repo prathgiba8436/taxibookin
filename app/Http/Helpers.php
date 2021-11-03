2ae0f8473bc6ac1c0529af4d7ae5a229c7b4b00e
Phan Hữu Phúc <46494875+huuphuc1999@users.noreply.github.com>
2021-11-03 16:44:10 +0700

    Update Helpers.php

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -7,7 +7,7 @@ use App\Models\Order;
 use App\Models\Wishlist;
 use App\Models\Shipping;
 use App\Models\Cart;
-// use Auth;
+namespace App\Http\Helpers;
 class Helper{
     public static function messageList()
     {
