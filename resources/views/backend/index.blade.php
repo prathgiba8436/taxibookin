b99b6a0cae2738d8011d7eab8901adf3c7576ef8
Phan Hữu Phúc <46494875+huuphuc1999@users.noreply.github.com>
2021-11-03 16:13:27 +0700

    Update index.blade.php

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -86,62 +86,17 @@
     </div>
     <div class="row">
 
-      <!-- Area Chart -->
-      <div class="col-xl-8 col-lg-7">
-        <div class="card shadow mb-4">
-          <!-- Card Header - Dropdown -->
-          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="m-0 font-weight-bold text-primary"></h6>
-            <img style="height: 100%; width: 100%;" src={{asset('backend/img/2.jpg')}} alt="">
-          </div>
-          <!-- Card Body -->
-          <div class="card-body">
-            
-            {{-- <div class="chart-area">
-              <canvas id="myAreaChart"></canvas>
-            </div> --}}
-          </div>
-        </div>
-      </div>
+
     
-      <!-- Pie Chart -->
-      <div class="col-xl-4 col-lg-5">
-        <div class="card shadow mb-4">
-          <!-- Card Header - Dropdown -->
-          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-            <h6 class="m-0 font-weight-bold text-primary">Người dùng</h6>
-          </div>
-          <!-- Card Body -->
-          <div class="card-body" style="overflow:hidden">
-            <div id="pie_chart" style="width:350px; height:320px;">
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Content Row -->
+
     
   </div>
 @endsection
 
 @push('scripts')
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
 {{-- pie chart --}}
-<script type="text/javascript">
-  var analytics = <?php echo $users; ?>
 
-  google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(drawChart);
-
-  function drawChart()
-  {
-      var data = google.visualization.arrayToDataTable(analytics);
-      var options = {
-          title : 'Last 7 Days registered user'
-      };
-      var chart = new google.visualization.PieChart(document.getElementById('pie_chart'));
-      chart.draw(data, options);
-  }
-</script>
 
 @endpush
